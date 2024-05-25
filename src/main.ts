4a3be5831041ecb60837b03fb2c7ac8648cbbbd7
nick-fite <70393136+nick-fite@users.noreply.github.com>
2024-05-25 03:38:40 -0500

    fixing image

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+const { BASE_URL } = import.meta.env;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   
@@ -6,7 +7,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="flex-container2">
       <div>
         <h1>Hi! I'm Nicholas Fite</h1>
-        <img src = "assets\\SmileyFace.png" alt ="Image of me">
+        <img src = "${BASE_URL}/assets/SmileyFace.png" alt ="Image of me">
         <div class="buttons-block">
           <button>About Me</button>
           <button>Skills</button>
