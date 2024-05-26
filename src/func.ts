2ea6c6bd0042fa5605937cdf6af5a63fbe1a82da
nick-fite <70393136+nick-fite@users.noreply.github.com>
2024-05-25 23:26:12 -0500

    added button functionality

---
+++ src/func.ts
@@ -0,0 +1,19 @@
+export function AboutMeClick(infoDiv : any)
+{
+    infoDiv!.innerHTML = `
+    <p id="ReadableSize">I'm a programmer and a game designer who's looking for the next step in his career</p>
+    `
+    return "test";
+}
+
+export function SkillsClick(infoDiv : any)
+{
+    infoDiv!.innerHTML = "Skills Test"
+    return "test";
+}
+
+export function ExperienceClick(infoDiv : any)
+{
+    infoDiv!.innerHTML = "Experience Test"
+    return "test";
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css';
+import { AboutMeClick, ExperienceClick, SkillsClick} from './func';
 const { BASE_URL } = import.meta.env;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -8,15 +9,26 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <div>
         <h1>Hi! I'm Nicholas Fite</h1>
         <img src = "${BASE_URL}SmileyFace.png" alt ="Image of me" id="PersonalImg">
-        <div class="buttons-block">
-          <button style="width: 100%;">About Me</button>
-          <button style="width: 100%;">Skills</button>
-          <button style="width: 100%;">Experience</button>
+        <div class="buttons-block" style="  padding-bottom: 5%;">
+          <input type="button" value="About Me" style="width: 100%;" id="AboutMeButton"></input>
+          <input type="button" value="Skills" style="width: 100%;" id="SkillsButton"></input>
+          <input type="button" value="Experience" style="width: 100%; " id="ExperienceButton"></input>
         </div>
       </div>
-      <div>
+      <div id="Information">
         <p id="ReadableSize">I'm a programmer and a game designer who's looking for the next step in his career</p>
       </div>
     </div>
   </div>
 `
+
+let infoDiv = document.getElementById("Information"); 
+
+let AboutMeBtn = document.getElementById("AboutMeButton");
+AboutMeBtn?.addEventListener("click", () => AboutMeClick(infoDiv));
+
+let SkillsBtn = document.getElementById("SkillsButton");
+SkillsBtn?.addEventListener("click", () => SkillsClick(infoDiv));
+
+let ExperienceBtn = document.getElementById("ExperienceButton");
+ExperienceBtn?.addEventListener("click", () => ExperienceClick(infoDiv));
