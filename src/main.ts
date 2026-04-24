b36d9a374c428777e009ba72ca9f4f691b34f128
Nick Fite <nicholasfite10@gmail.com>
2026-04-24 17:53:46 -0500

    attempted github action build

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css';
 import { AboutMeClick, EducationClick, ExperienceClick, SkillsClick} from './func.ts';
 const { BASE_URL } = import.meta.env;
 
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   
   <div class="flex-container">
